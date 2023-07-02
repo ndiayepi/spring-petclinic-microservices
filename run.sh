09c8d36d73b98cc0d082cb6106029d701b6f93d5
ndiayepi <pierrendiaye@gmail.com>
2023-07-02 18:42:31 +0200

    Ajout config vet service

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#../mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=native -DGIT_REPO=/localrepo"
-../mvnw spring-boot:run
+../mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=native -DGIT_REPO=/localrepo"
+#../mvnw spring-boot:run
