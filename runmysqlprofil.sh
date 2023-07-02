106264310a580a8f31b3b180ade243b414f55d73
ndiayepi <pierrendiaye@gmail.com>
2023-07-02 23:25:55 +0200

    Changes on application.yml in projects admi and api-gateway

--- runmysqlprofil.sh
+++ runmysqlprofil.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 #../mvnw spring-boot:run
-sleep 60
-../mvnw spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=mysql
+../mvnw spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=docker,mysql
